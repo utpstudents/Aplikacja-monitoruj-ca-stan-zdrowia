4f7e8cb49e5064273f2d7133558ee93e77824b60
Bartix5 <barti_x@op.pl>
2018-05-29 13:29:19 +0200

    Update Baza danych.sql

--- Baza danych.sql
+++ Baza danych.sql
@@ -3,10 +3,11 @@ CREATE DATABASE baza;
 USE baza;
 
 CREATE TABLE pomiary (
-id INT AUTO_INCREMENT PRIMARY KEY,
+id INT AUTO_INCREMENT,
 data DATE,
 temperatura FLOAT,
 cisnienie1 INT(3),
 cisnienie2 INT(3),
-tetno INT(3));
+tetno INT(3)
+PRIMARY KEY (id)); 
 
